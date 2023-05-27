712ea5a9462930fc59b5839bb3448c31b0f5cfe1
Hafiz Muhammad Tahir Nisar <tnisar786@yahoo.com>
2023-05-27 06:09:39 +0500

    Add files via upload

---
+++ Book chapter 34 and 35.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+customers = [
+{
+"customer id": 0,
+"first name":"John",
+"last name": "Ogden",
+"address": "301 Arbor Rd.",
+},
+{
+"customer id": 1,
+"first name":"Ann",
+"last name": "Sattermyer",
+"address": "PO Box 1145",
+},
+{
+"customer id": 2,
+"first name":"Jill",
+"last name": "Somers",
+"address": "3 Main St.",
+},
+]
+dictionary_to_look = customers[2]
+customer_info = dictionary_to_look["last name"]
+print(customer_info)
+print(len(customers))
+
+
+# In[21]:
+
+
+customers = [
+{
+"customer id": 0,
+"first name":"John",
+"last name": "Ogden",
+"address": "301 Arbor Rd.",
+},
+{
+"customer id": 1,
+"first name":"Ann",
+"last name": "Sattermyer",
+"address": "PO Box 1145",
+},
+{
+"customer id": 2,
+"first name":"Jill",
+"last name": "Somers",
+"address": "3 Main St.",
+},
+]
+
+print(customers)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
